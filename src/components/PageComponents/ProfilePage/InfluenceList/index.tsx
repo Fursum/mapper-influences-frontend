5fc6fb8871ad5505d9b9c972020a488c454d581f
Fursum <fursum0@gmail.com>
2022-06-12 15:12:27 +0300

    responsive profile page

--- src/components/PageComponents/ProfilePage/InfluenceList/index.tsx
+++ src/components/PageComponents/ProfilePage/InfluenceList/index.tsx
@@ -1,7 +1,7 @@
 import React, { FC } from "react";
 import { Influence } from "@libs/types/influence";
 
-import styles from "../profilePage.module.scss";
+import styles from "./style.module.scss";
 import InfluenceElement from "./InfluenceElement";
 
 type Props = { influences: Influence[] };
@@ -14,7 +14,12 @@ const InfluenceList: FC<Props> = ({ influences }) => {
     />
   ));
 
-  return <div className={styles.mapperInfluences}>{InfluenceCards}</div>;
+  return (
+    <div className={styles.mapperInfluences}>
+      <h2>Influences</h2>
+      {InfluenceCards}
+    </div>
+  );
 };
 
 export default InfluenceList;
