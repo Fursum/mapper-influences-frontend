40303ba225cd99a4037a7433c2fb13d5b7012af6
Fursum <fursum0@gmail.com>
2022-07-10 22:38:04 +0300

    remove bugged folder

--- src/components/svgComponents/index.tsx
+++
@@ -1,9 +0,0 @@
-import Followers from "./Followers";
-import Graved from "./Graved";
-import Loved from "./Loved";
-import Pending from "./Pending";
-import Ranked from "./Ranked";
-import Subscribers from "./Subscribers";
-import Influences from "./Influences";
-
-export { Followers, Graved, Loved, Pending, Ranked, Subscribers, Influences };
