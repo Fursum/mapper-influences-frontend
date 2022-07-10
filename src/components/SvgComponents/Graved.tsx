9e085898a0a06da7eee5ff9c70b118f218709bc9
Fursum <fursum0@gmail.com>
2022-07-10 22:36:38 +0300

    change git.ignorecase

---
+++ src/components/SvgComponents/Graved.tsx
@@ -0,0 +1,23 @@
+import { FC } from "react";
+
+type Props = { color?: string; className?: string };
+const Graved: FC<Props> = ({ color = "var(--textColor)", className }) => {
+  return (
+    <svg
+      className={className}
+      width="67"
+      height="74"
+      viewBox="0 0 67 74"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M67 33.5C67 33.667 66.9988 33.8336 66.9963 34H67V74H0V34H0.0036555C0.00122132 33.8336 0 33.667 0 33.5C0 14.9985 14.9985 0 33.5 0C52.0015 0 67 14.9985 67 33.5ZM28 18H38V30H48V39H38V62H28V39H18V30H28V18Z"
+        fill={color}
+      />
+    </svg>
+  );
+};
+export default Graved;
---
+++ src/components/SvgComponents/index.tsx
@@ -0,0 +1,9 @@
+import Followers from "./Followers";
+import Graved from "./Graved";
+import Loved from "./Loved";
+import Pending from "./Pending";
+import Ranked from "./Ranked";
+import Subscribers from "./Subscribers";
+import Influences from "./Influences";
+
+export { Followers, Graved, Loved, Pending, Ranked, Subscribers, Influences };
