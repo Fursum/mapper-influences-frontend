1574635a647ce3c792ebf4b660a1fff254ae34cd
Fursum <fursum0@gmail.com>
2022-08-01 22:56:59 +0300

    implement sun beams for light mode

--- src/components/Layout/Header/DarkModeToggle/index.tsx
+++ src/components/Layout/Header/DarkModeToggle/index.tsx
@@ -1,3 +1,4 @@
+import Beams from "@components/SvgComponents/Beams";
 import useDarkMode from "@hooks/useDarkMode";
 import React, { FC } from "react";
 
@@ -7,16 +8,21 @@ type Props = {
   className?: string;
 };
 
-const DarkModeToggle: FC<Props> = ({className}) => {
+const DarkModeToggle: FC<Props> = ({ className }) => {
   const [currentMode, setMode] = useDarkMode();
 
   const toggleMode = () => {
     setMode(currentMode === "dark" ? "light" : "dark");
   };
-
   return (
-    <button className={`${styles.outerSlider} ${className}`} onClick={toggleMode}>
-      <div className={`${styles.innerSlider} ${styles[currentMode]}`} />
+    <button
+      className={`${styles.outerSlider} ${className}`}
+      onClick={toggleMode}
+    >
+      <div className={`${styles.innerSlider} ${styles[currentMode]}`}>
+        <div className={styles.colorFill} />
+        <Beams className={styles.beam} />
+      </div>
     </button>
   );
 };
