d845a1ae805fa3489d7ec7ef5c13754771517c0d
Fursum <fursum0@gmail.com>
2022-08-06 19:25:10 +0300

    optimize text fit

--- src/components/SharedComponents/BaseProfileCard/index.tsx
+++ src/components/SharedComponents/BaseProfileCard/index.tsx
@@ -1,10 +1,9 @@
 import ProfilePhoto from "@components/SharedComponents/ProfilePhoto";
 import Link from "next/link";
-import React, { FC, useEffect } from "react";
+import React, { FC, useEffect, useState } from "react";
 import { UserBase } from "@libs/types/user";
 import Pill from "./Pill";
 import Image from "next/image";
-import useWindowSize from "@hooks/useWindowSize";
 
 import styles from "./style.module.scss";
 import AwesomeDebouncePromise from "awesome-debounce-promise";
@@ -18,18 +17,21 @@ const BaseProfileCard: FC<Props> = ({ userData }) => {
     <Pill key={group.id} group={group} />
   ));
 
+  const [randomDelay] = useState(Math.random() * 10)
+
   const runFitText = () =>
     textFit(document.getElementsByClassName(styles.name));
-  const debounceFitText = AwesomeDebouncePromise(runFitText, 2000);
+  // Random delay so every fit text does not run at once
+  const debounceFitText = AwesomeDebouncePromise(runFitText, 1000 + randomDelay);
 
-  const windowSize = useWindowSize();
-  // Fit text to card
+  // Fit text to card on resize
   useEffect(() => {
     if (document) runFitText();
+    window.addEventListener("resize", debounceFitText);
+    return () => {
+      window.removeEventListener("resize", debounceFitText);
+    };
   }, []);
-  useEffect(() => {
-    if (document) debounceFitText();
-  }, [windowSize]);
 
   return (
     <Link href={`/profile/${userData.id}`} passHref={true}>
