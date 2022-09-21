f6ec2306751b7be0620de7578fadcded8a3e7e68
Fursum <fursum0@gmail.com>
2022-09-22 02:07:14 +0300

    add third video

--- src/components/PageComponents/Home/LoginScreen/CoolCards/index.tsx
+++ src/components/PageComponents/Home/LoginScreen/CoolCards/index.tsx
@@ -3,34 +3,38 @@ import { FC } from "react";
 import styles from "./style.module.scss";
 
 const CoolCards: FC = () => {
+  const meta = [
+    {
+      url: "https://osu.ppy.sh/beatmapsets/1536948",
+      filePath: "/example1.webm",
+    },
+    {
+      url: "https://osu.ppy.sh/beatmapsets/1846040",
+      filePath: "/example2.webm",
+    },
+    {
+      url: "https://osu.ppy.sh/beatmapsets/855677",
+      filePath: "/example3.webm",
+    },
+  ];
+
   return (
     <div className={styles.positioner}>
       <div className={styles.cardWrapper}>
-        <a
-          href="https://osu.ppy.sh/beatmapsets/1536948"
-          target={"_blank"}
-          rel="noreferrer"
-        >
-          <video autoPlay muted loop playsInline>
-            <source src="/example1.webm" type="video/webm" />
-          </video>
-          <div className={styles.overlay}>Go to map</div>
-          <div className={styles.shadow} />
-        </a>
-        <a
-          href="https://osu.ppy.sh/beatmapsets/1846040"
-          target={"_blank"}
-          rel="noreferrer"
-        >
-          <video autoPlay muted loop playsInline>
-            <source src="/example2.webm" type="video/webm" />
-          </video>
-          <div className={styles.overlay}>Go to map</div>
-          <div className={styles.shadow} />
-        </a>
-        <div>
-          card3 <div className={styles.shadow} />
-        </div>
+        {meta.map((item, i) => (
+          <a
+            key={item.url + i}
+            href={item.url}
+            target={"_blank"}
+            rel="noreferrer"
+          >
+            <video autoPlay muted loop playsInline>
+              <source src={item.filePath} type="video/webm" />
+            </video>
+            <div className={styles.overlay} />
+            <div className={styles.shadow} />
+          </a>
+        ))}
       </div>
     </div>
   );
