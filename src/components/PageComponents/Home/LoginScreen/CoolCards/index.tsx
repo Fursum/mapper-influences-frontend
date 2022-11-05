12db2de7b149684d7d9f5b398c9d3c81b22a2f25
Fursum <fursum0@gmail.com>
2022-11-05 18:45:13 +0300

    remove vercel videos and use discord cdn instead

--- src/components/PageComponents/Home/LoginScreen/CoolCards/index.tsx
+++ src/components/PageComponents/Home/LoginScreen/CoolCards/index.tsx
@@ -6,15 +6,18 @@ const CoolCards: FC = () => {
   const meta = [
     {
       url: "https://osu.ppy.sh/beatmapsets/1536948",
-      filePath: "/example1.webm",
+      filePath:
+        "https://cdn.discordapp.com/attachments/645591556585291776/1038477819912650792/example1.webm",
     },
     {
       url: "https://osu.ppy.sh/beatmapsets/1846040",
-      filePath: "/example2.webm",
+      filePath:
+        "https://cdn.discordapp.com/attachments/645591556585291776/1038477820751515789/example2.webm",
     },
     {
       url: "https://osu.ppy.sh/beatmapsets/855677",
-      filePath: "/example3.webm",
+      filePath:
+        "https://cdn.discordapp.com/attachments/645591556585291776/1038477820399210526/example3.webm",
     },
   ];
 
