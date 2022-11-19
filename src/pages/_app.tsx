3c33669109380227fe25bf31f6307550745c09aa
Fursum <fursum0@gmail.com>
2022-11-19 11:22:50 +0300

    add metadata

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,8 +7,19 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <meta name="description" content="" />
-        <meta name="keywords" content="" />
+        <meta
+          name="description"
+          content="Track and share your osu! mapping influences."
+        />
+        <meta
+          name="keywords"
+          content="osu, mapping, map, beatmap, beatmaps, community, influence, style"
+        />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0"
+        ></meta>
+        <meta name="author" content="Fursum"></meta>
         <link rel="icon" href="/svg/Influences.svg" />
         <title>Mapper Influences</title>
       </Head>
