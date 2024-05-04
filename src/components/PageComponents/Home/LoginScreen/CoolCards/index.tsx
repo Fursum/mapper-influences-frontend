312c7ad6a90b856542f1f6371cef7ed35e045260
Fursum <fursum0@gmail.com>
2024-05-04 19:20:04 +0300

    fix loop

--- src/components/PageComponents/Home/LoginScreen/CoolCards/index.tsx
+++ src/components/PageComponents/Home/LoginScreen/CoolCards/index.tsx
@@ -32,7 +32,7 @@ const CoolCards: FC = () => {
             <iframe
               width="100%"
               height="100%"
-              src={`https://www.youtube.com/embed/${item.video}?autoplay=1&controls=0&mute=1&loop=1`}
+              src={`https://www.youtube.com/embed/${item.video}?autoplay=1&controls=0&mute=1&loop=1&showinfo=0&rel=0&playlist=${item.video}`}
               frameBorder="0"
               allow="autoplay"
             />
