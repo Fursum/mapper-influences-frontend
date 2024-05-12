d7d23773f6183512d8e0f4b6de0cf7582b65b574
Fursum <fursum0@gmail.com>
2024-05-13 02:31:15 +0300

    upgrade packages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,26 @@
 import "../styles/globals.scss";
 
 import Layout from "@components/Layout";
-import {
-  Hydrate,
-  QueryClient,
-  QueryClientProvider,
-} from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import { useState } from "react";
 import { CookiesProvider } from "react-cookie";
 
-const queryClient = new QueryClient();
-
 function App({ Component, pageProps }: AppProps) {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            // With SSR, we usually want to set some default staleTime
+            // above 0 to avoid refetching immediately on the client
+            staleTime: 60 * 1000,
+          },
+        },
+      })
+  );
+
   return (
     <>
       <Head>
@@ -24,10 +32,8 @@ function App({ Component, pageProps }: AppProps) {
           name="keywords"
           content="osu, mapping, map, beatmap, beatmaps, community, influence, style"
         />
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1.0"></meta>
-        <meta name="author" content="Fursum"></meta>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="author" content="Fursum" />
         <link rel="icon" href="/svg/Influences.svg" />
         <link rel="shortcut icon" href="/svg/Influences.svg" />
         <link rel="mask-icon" href="/svg/Influences.svg" color="#000000" />
@@ -35,13 +41,11 @@ function App({ Component, pageProps }: AppProps) {
       </Head>
 
       <QueryClientProvider client={queryClient}>
-        <Hydrate state={pageProps.dehydratedState}>
-          <CookiesProvider>
-            <Layout>
-              <Component {...pageProps} />
-            </Layout>
-          </CookiesProvider>
-        </Hydrate>
+        <CookiesProvider>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </CookiesProvider>
       </QueryClientProvider>
     </>
   );
