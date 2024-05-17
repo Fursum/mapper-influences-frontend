6f8b945ddbe9ee99697991bcff5c111d3dee63d4
Fursum <fursum0@gmail.com>
2024-05-17 04:22:49 +0300

    fix badge spacing

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,14 +1,11 @@
-import ProfilePage from "@components/PageComponents/ProfilePage";
-import useAuth from "@hooks/useAuth";
-import type { NextPage } from "next";
+import ProfilePage from '@components/PageComponents/ProfilePage';
+import useAuth from '@hooks/useAuth';
+import type { NextPage } from 'next';
 
 const MapperPage: NextPage = () => {
   useAuth();
 
-  //if (error) return <h1>Error while fetching user: {error}</h1>;
-  //if (loading) return <span>Loading...</span>;
   return <ProfilePage />;
-  //return <h1>User not found! {error}</h1>;
 };
 
 export default MapperPage;
