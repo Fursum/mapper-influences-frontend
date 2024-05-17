bdf0b88aff8da7d1f38069ce4bb60cc3e00455f2
Fursum <fursum0@gmail.com>
2024-05-17 12:23:38 +0300

    try prefetching to load css

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,9 +1,18 @@
+import { useEffect } from 'react';
+
 import ProfilePage from '@components/PageComponents/ProfilePage';
 import useAuth from '@hooks/useAuth';
 import type { NextPage } from 'next';
+import { useRouter } from 'next/router';
 
 const MapperPage: NextPage = () => {
+  const router = useRouter();
+
   useAuth();
+
+  useEffect(() => {
+    router.prefetch('/dashboard');
+  }, []);
 
   return <ProfilePage />;
 };
