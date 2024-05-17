eaef47f81371556710caae80653a8cd2348f7090
Fursum <fursum0@gmail.com>
2024-05-17 04:52:29 +0300

    close search after clicking on a profile

--- src/components/Layout/Header/SearchBar/index.tsx
+++ src/components/Layout/Header/SearchBar/index.tsx
@@ -1,9 +1,10 @@
-import { type FC, useCallback, useRef, useState } from 'react';
+import { type FC, useCallback, useEffect, useRef, useState } from 'react';
 
 import { Magnify } from '@components/SvgComponents';
 import { MaxNameLength } from '@libs/consts';
 import { getSearchResults } from '@services/search';
 import AwesomeDebouncePromise from 'awesome-debounce-promise';
+import { useRouter } from 'next/router';
 import type { UserCompact } from 'osu-web.js';
 import { useOnClickOutside } from 'usehooks-ts';
 
@@ -16,12 +17,19 @@ type Props = {
 };
 
 const SearchBar: FC<Props> = ({ className }) => {
+  const router = useRouter();
   const containerRef = useRef(null);
   const inputRef = useRef<HTMLInputElement>(null);
   const [results, setResults] = useState<UserCompact[] | undefined>();
   const [showResults, setShowResults] = useState(false);
 
   useOnClickOutside(containerRef, () => setShowResults(false));
+
+  // Close search results when navigating to a new page
+  // biome-ignore lint/correctness/useExhaustiveDependencies: <explanation above>
+  useEffect(() => {
+    setShowResults(false);
+  }, [router.asPath]);
 
   const searchUser = useCallback((query: string) => {
     getSearchResults(query).then((res) => {
