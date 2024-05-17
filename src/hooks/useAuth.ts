e64226bf581085ed0b77a830a13a460a2187f38f
Fursum <fursum0@gmail.com>
2024-05-18 00:36:33 +0300

    add a message when failing to login

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,6 +1,8 @@
 import { useCallback, useEffect } from 'react';
+import { toast } from 'react-toastify';
 
 import { useRouter } from 'next/router';
+import { useIsClient } from 'usehooks-ts';
 
 import { useCurrentUser } from './useUser';
 
@@ -8,13 +10,19 @@ const REDIRECT_URL = '/';
 
 const useAuth = () => {
   const router = useRouter();
-  const { data: user, isLoading } = useCurrentUser();
+  const { data: user, isLoading, error, errorUpdatedAt } = useCurrentUser();
+  const isClient = useIsClient();
 
   // Manual logout
   const logout = useCallback(() => {
     // Logout endpoint
     router.push(REDIRECT_URL); // Wont work yet
   }, [router.push]);
+
+  useEffect(() => {
+    if (error && errorUpdatedAt && isClient && router.asPath === '/')
+      toast.error('Failed to log in.');
+  }, [errorUpdatedAt]);
 
   // Kick user to home page if not logged in
   // or redirect to dashboard if logged in
