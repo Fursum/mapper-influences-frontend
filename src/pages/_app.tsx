656cae796c34037e2fe9e066647dd76e323f4359
Fursum <fursum0@gmail.com>
2024-05-18 08:08:53 +0300

    reduce default retry count

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,12 @@
-import "../styles/globals.scss";
+import { useState } from 'react';
+import { CookiesProvider } from 'react-cookie';
 
-import Layout from "@components/Layout";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import type { AppProps } from "next/app";
-import Head from "next/head";
-import { useState } from "react";
-import { CookiesProvider } from "react-cookie";
+import Layout from '@components/Layout';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+
+import '../styles/globals.scss';
 
 function App({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(
@@ -13,12 +14,11 @@ function App({ Component, pageProps }: AppProps) {
       new QueryClient({
         defaultOptions: {
           queries: {
-            // With SSR, we usually want to set some default staleTime
-            // above 0 to avoid refetching immediately on the client
             staleTime: 60 * 1000,
+            retry: 1,
           },
         },
-      })
+      }),
   );
 
   return (
