7507800d6b583620cc570230cacbb78eae5f02ad
Fursum <fursum0@gmail.com>
2024-05-20 05:55:42 +0300

    Revert "try new meta properties"
    This reverts commit adc2bc30e4779911e0b6e660f081e3a96af28528.

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,16 +1,19 @@
 import ProfilePage from '@components/PageComponents/ProfilePage';
 import useAuth from '@hooks/useAuth';
+import { useCurrentUser } from '@services/user';
 import type { NextPage } from 'next';
 import Head from 'next/head';
 
 const MapperPage: NextPage = () => {
   useAuth();
 
+  const { data: currentUser } = useCurrentUser();
+
   return (
     <>
       <Head>
-        <title>{'Mapper Influences'}</title>
         <meta name="description" content={'Your own profile page.'} />
+        <title>{`${currentUser?.username} - Mapper Influences`}</title>
       </Head>
       <ProfilePage />
     </>
