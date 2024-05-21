e1bbacf72d9ff1050b01e5f3c1c567d86fdff05c
Fursum <fursum0@gmail.com>
2024-05-21 07:17:24 +0300

    add infinite scroll to mentions

--- src/components/PageComponents/ProfilePage/MentionList/index.tsx
+++ src/components/PageComponents/ProfilePage/MentionList/index.tsx
@@ -1,7 +1,8 @@
-import type { FC } from 'react';
+import { type FC, useEffect, useState } from 'react';
+import InfiniteScroll from 'react-infinite-scroller';
 
 import BaseProfileCard from '@components/SharedComponents/BaseProfileCard';
-import { useGetMentions } from '@services/influence';
+import { type InfluenceResponse, useGetMentions } from '@services/influence';
 
 import styles from './style.module.scss';
 
@@ -9,20 +10,38 @@ type Props = { open?: boolean; userId?: number | string };
 const MentionList: FC<Props> = ({ open, userId }) => {
   const { data: mentions } = useGetMentions(userId);
 
+  const [visibleMentions, setVisibleMentions] = useState<InfluenceResponse[]>(
+    [],
+  );
+
+  useEffect(() => {
+    setVisibleMentions(mentions?.slice(0, 10) || []);
+  }, [mentions]);
+
   return (
     <div
       className={styles.mentionList}
       style={!open ? { display: 'none' } : {}}
     >
       {!mentions?.length && <span>{'No mentions :('}</span>}
-      <div className={styles.mentionGrid}>
-        {mentions?.map((mention) => (
+      <InfiniteScroll
+        initialLoad={true}
+        className={styles.mentionGrid}
+        loadMore={() =>
+          mentions &&
+          visibleMentions.length < mentions?.length &&
+          setVisibleMentions(mentions?.slice(0, visibleMentions.length + 10))
+        }
+        hasMore={mentions && mentions.length > visibleMentions.length}
+        useWindow={true}
+      >
+        {visibleMentions?.map((mention) => (
           <BaseProfileCard
             key={mention.influenced_by}
             userId={mention.influenced_by}
           />
         ))}
-      </div>
+      </InfiniteScroll>
     </div>
   );
 };
