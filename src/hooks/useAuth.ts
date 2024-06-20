15982b52c5429be740abf7bb0556673f2759442b
Fursum <fursum0@gmail.com>
2024-06-20 06:20:30 +0300

    fix useAuth redirects and add a redirect cache

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -4,6 +4,7 @@ import { toast } from 'react-toastify';
 import { logoutRequest, useCurrentUser } from '@services/user';
 import { useQueryClient } from '@tanstack/react-query';
 import { useRouter } from 'next/router';
+import { useLocalStorage } from 'usehooks-ts';
 
 const REDIRECT_URL = '/';
 
@@ -11,12 +12,16 @@ const useAuth = () => {
   const queryClient = useQueryClient();
   const router = useRouter();
   const { data: user, isLoading, error } = useCurrentUser();
+  const [redirectUrl, setRedirectUrl] = useLocalStorage<string | null>(
+    'redirect',
+    null,
+  );
 
   // Manual logout
   const logout = useCallback(() => {
     logoutRequest()
       .then(() => {
-        queryClient.invalidateQueries({
+        queryClient.resetQueries({
           queryKey: ['currentUser'],
         });
         router.push(REDIRECT_URL);
@@ -24,21 +29,29 @@ const useAuth = () => {
       .catch(() => {
         toast.error('Failed to logout. 🤔');
       });
-  }, [router.push, queryClient.invalidateQueries]);
+  }, [router.push, queryClient.resetQueries]);
 
   // Kick user to home page if not logged in
   // or redirect to dashboard if logged in
   useEffect(() => {
     if (isLoading) return;
+
+    if (user && redirectUrl) {
+      router.push(redirectUrl);
+      setRedirectUrl(null);
+      return;
+    }
+
     if (router.asPath === REDIRECT_URL) {
       if (!user || error) return;
       router.push('/dashboard');
     }
 
     if (!user || error) {
+      setRedirectUrl(router.asPath);
       router.push(REDIRECT_URL);
     }
-  }, [isLoading, router, user, error]);
+  }, [isLoading, router, user, error, redirectUrl]);
 
   return { logout };
 };
